60ab59f9c403c8f99a5ffc04095d3236bafb2452
심재성 <tlawotjd4285@gmail.com>
2024-01-22 01:37:38 +0900

    20240122

---
+++ 기초 코드PS/10093.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	long long a, b;
+	cin >> a >> b;
+	if (a > b) swap(a, b);
+
+	if (a == b)cout << 0 << '\n';	
+	else cout << b - a - 1 << '\n';
+
+	for (long long i = a +1; i < b; i++)	cout << i << ' ';
+	
+	return 0;
+}
---
+++ 기초 코드PS/2587.cpp
@@ -0,0 +1,22 @@
+//#include<iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int arr[5], sum = 0;
+//
+//	for (int i = 0; i < 5; i++) {
+//		cin >> arr[i];
+//		sum += arr[i];
+//	}
+//	for (int i = 0; i < 5; i++) {
+//		int tmp = i;
+//		for (int j = i+1; j < 5; j++) {
+//			if (arr[i] > arr[j]) {
+//				swap(arr[i], arr[j]);
+//			}
+//		}
+//	}
+//	cout << sum / 5 << '\n' << arr[2];
+//	return 0;
+//}
